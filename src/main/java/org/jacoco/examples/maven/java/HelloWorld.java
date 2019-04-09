3209e54c5a0a1d89f96c6604f7faf730ad85da8c
GirishGit29 <49023572+GirishGit29@users.noreply.github.com>
2019-04-09 17:41:14 +0530

    Update HelloWorld.java

--- src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -4,9 +4,9 @@ public class HelloWorld {
 	
 	public String getMessage(boolean bigger) {
 		if (bigger) {
-			return "Hello Universe!";
+			return "Hello Universe!!!";
 		} else {
-			return "Hello World!";
+			return "Hello World!!!";
 		}
 	}
 
